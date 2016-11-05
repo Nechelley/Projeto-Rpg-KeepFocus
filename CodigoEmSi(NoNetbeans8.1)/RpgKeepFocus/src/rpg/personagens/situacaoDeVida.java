38b08f4215f3106400bec696364e9299353813f5
Nechelley <Nechelley@live.com>
2016-11-04 22:24:38 -0200

    arrumando ainda

--- CodigoEmSi(NoNetbeans8.1)/RpgKeepFocus/src/rpg/personagens/situacaoDeVida.java
+++ CodigoEmSi(NoNetbeans8.1)/RpgKeepFocus/src/rpg/personagens/situacaoDeVida.java
@@ -1,15 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package rpg.personagens;
 
 /**
+ * Representa as condicoes de vida que o personagem pode estar, com base no dano recebido
+ * 
+ * 0-Saudável ou 1-Atordoado ou 2-Desesperado ou 3-Inconsciente ou 4-Morto
+ * 20% dano      40% dano       60% dano         99% dano          100% dano
  * 
  * @author Nechelley Alves
  */
-public enum situacaoDeVida {
+public enum SituacaoDeVida {
+    SAUDAVEL(0,"Saudável",0.2),ATORDOADO(1,"Atordoado",0.4),DESESPERADO(2,"Desesperado",0.6),
+    INCONSCIENTE(3,"Inconsciente",0.99),MORTO(4,"Morto",1);
+    
+    private final int id;
+    private final String nome;
+    private final double porcentagem;
+    
+    private SituacaoDeVida(int id, String nome, double porcentagem){
+        this.id = id;
+        this.nome = nome;
+        this.porcentagem = porcentagem;
+    }
+    
+    /**
+     * @return Int com o id da situacao de vida
+     */
+    public int getId(){
+        return id;
+    }
+    
+    /**
+     * @return Double com a porcentagem da situacao de vida
+     */
+    public double getPorcentagem(){
+        return porcentagem;
+    }
 
+    /**
+     * @return String com o nome da situacao de vida
+     */
+    public String getString(){
+        return nome;
+    }
 }
